e36a4606434d261667f461f6ca384d88f2ae1976
rahulk123-cmd <155637389+Rahulkalagadda@users.noreply.github.com>
2026-04-27 13:57:18 +0530

    shrinkresource false

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
